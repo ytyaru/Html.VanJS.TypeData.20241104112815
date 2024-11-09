2a6511b4b8316c4a5428244bc9429ce3af5437ac
ytyaru <yttry0@gmail.com>
2024-11-09 15:03:25 +0900

    途中

--- docs/1/js/typedata.js
+++ docs/1/js/typedata.js
@@ -34,6 +34,64 @@ class TypeData {
     #isNullable(n) {return n.endsWith('?') || n.endsWith('?=')}
     #isMutable(n) {return n.endsWith('=') || n.endsWith('?=')}
     #isGenericable(n) {return n.includes('<') && n.includes('>')}
+    #read(s,l) { // 文字列sを解析してTypeDataを生成する。配列lがあればその末尾に加える
+        s = s.replace(/ /g,'') // 半角スペースを全削除する
+        const idx = {
+            comma: s.indexOf(','),
+            less: s.indexOf('<'),
+            great: s.lastIndexOf('>'),
+        }
+        if (0<=idx.less && 0<=idx.great && idx.great < idx.less) {throw new TypeError(`ジェネリクスの記号順が不正です。'<>'であるべき所が'><'になっています。`)}
+        if ((0<=idx.less && -1===idx.great) || (-1===idx.less && 0<=idx.great)) {throw new TypeError(`ジェネリクスの記号が不足しています。'<>'であるべき所が'<'か'>'の片方しかありません。`)}
+
+        if (-1===idx.comma && -1===idx.less && -1===idx.great) { // int?=
+            //return {name:s, nullable:this.#isNullable(s), mutable:this.#isMutable(s), generics:null} 
+            const o = {name:s, nullable:this.#isNullable(s), mutable:this.#isMutable(s), generics:null} 
+            if (Type.isAry(l)) {l.push(o)}
+            return l ? l : o
+        }
+        else if (-1===idx.comma && 0<=idx.less && 0<=idx.great) { // ary?=<int?=>   ary?=<ary?=<int?=>>
+            const base = s.slice(0, s.indexOf(idx.less))
+            const gen = s.slice(s.indexOf(idx.less), s.indexOf(idx.great))
+//            return {name:base, nullable:this.#isNullable(base), mutable:this.#isMutable(base), generics:this.#read(gen)} 
+            const o = {name:base, nullable:this.#isNullable(base), mutable:this.#isMutable(base), generics:this.#read(gen)} 
+            if (Type.isAry(l)) {l.push(o)}
+            return l ? l : o
+        }
+        else if (0<=idx.comma && -1===idx.less && -1===idx.great) { // int?=,str?=
+            return s.split(',').map(t=>this.#read(t.trim()))
+        }
+        else if (0<=idx.comma && 0<=idx.less && 0<=idx.great) { kvs?=<int?=,str?=>  kvs?=<int?=,ary?=<str?=>>  ary?=<str?=>,int?=
+            if (idx.comma < idx.less) { // int?=,kvs<str?=,int?=>
+                const datas = []
+                const name = s.slice(0,idx.comma)
+                datas.push({name:name, nullable:this.#isNullable(name), mutable:this.#isMutable(name), generics:null})
+                return this.#read(s.slice(idx.comma), datas)
+            } else if (idx.less < idx.comma) { // kvs<str?=,int?=>  ary<int>,str  kvs<str?=,int?=>,str?=
+                const base = s.slice(0, idx.less)
+                const t = s.slice(idx.less)
+                if (','===s.slice(idx.great, idx.great + 1) { // 同位配列  ary<int>,str
+                    const datas = []
+                    const name = s.slice(0,idx.comma)
+                    datas.push({name:base, nullable:this.#isNullable(base), mutable:this.#isMutable(base), generics:t.slice(0,idx.great)})
+                    return this.#read(t.slice(idx.great+1), datas)
+                } else { // 下位配列 kvs<str?=,int?=>
+                    const o = {name:base, nullable:this.#isNullable(base), mutable:this.#isMutable(base), generics:this.#read(t.slice(0,idx.great))} 
+                    if (Type.isAry(l)) {l.push(o)}
+                    return l ? l : o
+                }
+                if (t.includes('>,')) { // 同位配列? kvs<kvs<int?=,str?=>,kvs<str?=,int?=>>
+                    
+                } else {
+
+                }
+                if (t.endsWith('>'))
+            }
+        }
+        if (0<=idx.less && idx.less < idx.comma) { // ary<int>,str
+            s.slice(0, s.indexOf(idx.less))
+        }
+    }
     #baseName(name) {
         let n = name
         const nullable = this.#isNullable(n)
