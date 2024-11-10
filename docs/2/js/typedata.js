250a5d3b2817fa35b7cc5e1117ef2636a6156513
ytyaru <yttry0@gmail.com>
2024-11-10 12:16:59 +0900

    2 途中

--- docs/2/js/typedata.js
+++ docs/2/js/typedata.js
@@ -1,6 +1,7 @@
 class TypeData {
     static read(s,l) { // 文字列sを解析してTypeDataを生成する。配列lがあればその末尾に加える
-        s = s.replace(/ /g,'') // 半角スペースを全削除する
+        //s = s.replace(/ /g,'') // 半角スペースを全削除する
+        s = s.replace(/\s/g,'') // 空白文字（半角スペース、改行、タブ）を全削除する
         const idx = {
             comma: s.indexOf(','),
             less: s.indexOf('<'),
@@ -43,26 +44,45 @@ class TypeData {
             if (idx.comma < idx.less) { // int?=,kvs<str?=,int?=>
                 const datas = []
                 const name = s.slice(0,idx.comma)
+                console.log(name)
+                console.log(s.slice(idx.comma+1))
 //                datas.push({name:name, nullable:this.#isNullable(name), mutable:this.#isMutable(name), generics:null})
                 datas.push(this.#makeTypeObj(name, null))
-                return this.read(s.slice(idx.comma), datas)
+                //return this.read(s.slice(idx.comma), datas)
+                return this.read(s.slice(idx.comma+1), datas)
             //} else if (idx.less < idx.comma) { // kvs<str?=,int?=>  ary<int>,str  kvs<str?=,int?=>,str?=
             } else { // kvs<str?=,int?=>  ary<int>,str  kvs<str?=,int?=>,str?=
                 const base = s.slice(0, idx.less)
                 const t = s.slice(idx.less+1, idx.greatL)
+                console.log(s)
                 console.log(base)
                 console.log(t)
-                const great = s.indexOf('>')
+                console.log(s.slice(idx.greatF+1, idx.greatF+2))
+                console.log(t.slice(0,idx.greatF))
+                console.log(s.slice(idx.greatF+2))
+                console.log(s.slice(idx.greatL+2))
+//                const great = s.indexOf('>')
                 //if (','===s.slice(idx.great, idx.great + 1) { // 同位配列  ary<int>,str
-                if (','===s.slice(idx.greatF+1, idx.greatF+2)) { // 同位配列  ary<int>,str
+                //if (','===s.slice(idx.greatF+1, idx.greatF+2)) { // 同位配列  >,   ary<int>,str  x kvs<ary<int>,str>
+                if (','===s.slice(idx.greatL+1, idx.greatL+2)) { // 同位配列  >,   ary<int>,str  x kvs<ary<int>,str>
                     const datas = []
                     const name = s.slice(0,idx.comma)
+                    console.log(name)
+                    console.log(t)
+                    console.log(t.slice(0,idx.greatF))
 //                    datas.push({name:base, nullable:this.#isNullable(base), mutable:this.#isMutable(base), generics:t.slice(0,idx.greatF)})
-                    datas.push(this.#makeTypeObj(base, t.slice(0,idx.greatF)))
-                    return this.read(t.slice(idx.greatF+2), datas)
+                    //datas.push(this.#makeTypeObj(base, t.slice(0,idx.greatF)))
+                    //datas.push(this.#makeTypeObj(base, this.read(t.slice(0,idx.greatF))))
+                    datas.push(this.#makeTypeObj(base, this.read(t)))
+                    //return this.read(t.slice(idx.greatF+2), datas)
+                    //return this.read(s.slice(idx.greatF+2), datas)
+                    return this.read(s.slice(idx.greatL+2), datas)
                 } else { // 下位配列 kvs<str?=,int?=>
                     //const o = {name:base, nullable:this.#isNullable(base), mutable:this.#isMutable(base), generics:this.read(t.slice(0,idx.greatF))} 
-                    const o = this.#makeTypeObj(base, this.read(t.slice(0,idx.greatF)))
+                    //const o = this.#makeTypeObj(base, this.read(t.slice(0,idx.greatF)))
+                    console.log(base)
+                    console.log(t.slice(0,idx.greatL))
+                    const o = this.#makeTypeObj(base, this.read(t.slice(0,idx.greatL)))
                     if (Type.isAry(l)) {l.push(o)}
                     return l ? l : o
                 }
